bf8f8d65a1e03b3b1f3b142ece419cb63411eaec
Casper da Costa-Luis <casper.dcl@physics.org>
2023-02-06 17:20:23 +0000

    warn on not found

--- fetch.sh
+++ fetch.sh
@@ -5,9 +5,17 @@ for repo in "${REPOS[@]}"; do
   fname="${repo//\//-}.json"
   # fname="$(basename $repo).json"
   echo >&2 "$repo -> {issues,prs}.$fname"
-  [ ! -f issues.$fname ] && \
-  gh api -H "Accept: application/vnd.github+json" --paginate /repos/${repo}/issues?state=open > issues.$fname && \
-  gh api -H "Accept: application/vnd.github+json" --paginate /repos/${repo}/pulls?state=open > prs.$fname
-  # gh issue list -R ${repo} -L 98765 --json comments,createdAt,labels,reactionGroups,updatedAt,url,assignees,title > issues.$fname && \
-  # gh pr list -R ${repo} -L 98765 --json comments,createdAt,labels,reactionGroups,updatedAt,url,assignees,title > prs.$fname
+  if test ! -f issues.$fname; then
+    gh api -H "Accept: application/vnd.github+json" --paginate /repos/${repo}/issues?state=open > issues.$fname && \
+    gh api -H "Accept: application/vnd.github+json" --paginate /repos/${repo}/pulls?state=open > prs.$fname || (
+      if test -n "$GITHUB_STEP_SUMMARY"; then
+        echo "Could not load $repo" >> $GITHUB_STEP_SUMMARY
+      else
+        echo >&2 "Could not load $repo"
+      fi
+      rm -f {issues,prs}.$fname
+    )
+  fi
+  # CMD="list -R ${repo} -L 98765 --json comments,createdAt,labels,reactionGroups,updatedAt,url,assignees,title"
+  # gh issue $CMD > issues.$fname && gh pr list $CMD > prs.$fname || rm -f {issues,prs}.$fname
 done
