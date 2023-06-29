9dff6430fcbcc789a3909947b0e645f2da808b73
Casper da Costa-Luis <casper.dcl@physics.org>
2023-06-29 16:45:13 +0100

    Revert "Merge pull request #2 from iterative/fix-fine-grained-auth"
    This reverts commit 7e4c05666efb7e65acc6fbb5a37e13745ebba741, reversing
    changes made to 185af16f86ce773686c7b22011d5259c07ed1cb6.
    
    Due to https://github.com/cli/cli/issues/6680 being resolved

--- fetch.sh
+++ fetch.sh
@@ -4,19 +4,18 @@ REPOS=("$@")
 for repo in "${REPOS[@]}"; do
   fname="${repo//\//-}.json"
   # fname="$(basename $repo).json"
-  echo >&2 "$repo -> issues.$fname"
-  if test ! -f issues.$fname; then
-    # also contains PRs
-    gh api -H "Accept: application/vnd.github+json" --paginate /repos/${repo}/issues?state=open > issues.$fname || (
-      if test -n "$GITHUB_STEP_SUMMARY"; then
-        echo "::error file=$0,line=10,title=could not load::token likely missing read access for https://github.com/$repo"
-      else
-        echo >&2 "Could not load $repo"
-      fi
-      rm -f issues.$fname
-    )
-  fi
-  # GraphQL (doesn't work with fine-grained tokens, vis. https://github.com/iterative/priority-list/issues/1)
-  # CMD="list -R ${repo} -L 98765 --json comments,createdAt,labels,reactionGroups,updatedAt,url,assignees,title"
-  # gh issue $CMD > issues.$fname && gh pr list $CMD > prs.$fname || rm -f {issues,prs}.$fname
+  CMD="list -R ${repo} -L 98765 --json comments,createdAt,labels,reactionGroups,updatedAt,url,assignees,title"
+  echo >&2 "$repo -> {issue,pr}s.$fname"
+  for TYP in issue pr; do
+    if test ! -f ${TYP}s.$fname; then
+      gh $TYP $CMD > ${TYP}s.$fname || (
+        if test -n "$GITHUB_STEP_SUMMARY"; then
+          echo "::error file=$0,line=12,title=could not load::token likely missing read access for https://github.com/$repo"
+        else
+          echo >&2 "Could not load $repo"
+        fi
+        rm -f ${TYP}s.$fname
+      )
+    fi
+  done
 done
